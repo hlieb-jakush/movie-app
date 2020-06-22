f5ab262e9e8c81af4f4b33476ce53eee001cf84a
Gleb Yakush <sivanv99@gmail.com>
2020-06-22 15:59:18 +0300

    Updates

---
+++ precache-manifest.e48269a5f9a720001e77f22da6a2171c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c3c1e0373ca320b4ab724950c29d9a",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "e2a4e364ecba9a26810e",
+    "url": "/movie-app/static/css/main.ce345b44.chunk.css"
+  },
+  {
+    "revision": "56aac0a68d87626d1891",
+    "url": "/movie-app/static/js/2.26608836.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-app/static/js/2.26608836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a4e364ecba9a26810e",
+    "url": "/movie-app/static/js/main.901ec545.chunk.js"
+  },
+  {
+    "revision": "255bee9b5e55438d0d88",
+    "url": "/movie-app/static/js/runtime-main.4f650522.js"
+  },
+  {
+    "revision": "17b75e4d707685368654b1c5c1a6ace3",
+    "url": "/movie-app/static/media/calendar.17b75e4d.svg"
+  },
+  {
+    "revision": "1caaef75e605ef2dc6d1a59b72791a09",
+    "url": "/movie-app/static/media/left.1caaef75.svg"
+  },
+  {
+    "revision": "1ae09dda7bf73c582f262b80bad5be09",
+    "url": "/movie-app/static/media/logo.1ae09dda.svg"
+  },
+  {
+    "revision": "9a9e9d1853acc256d896b5ce55c8b8fe",
+    "url": "/movie-app/static/media/right.9a9e9d18.svg"
+  },
+  {
+    "revision": "95816c751f0ddacce257e9688354e61f",
+    "url": "/movie-app/static/media/settings.95816c75.svg"
+  }
+]);
