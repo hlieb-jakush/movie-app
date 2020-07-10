7982b72ea713f689e6b2a9e4d316d2ea71e816e6
Gleb Yakush <sivanv99@gmail.com>
2020-07-10 15:06:02 +0300

    feat: add app routing and connect app.js to store (2)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 import Header from './components/Header/Header'
 import SearchSectionContainer from './components/SearchSection/SearchSectionContainer'
 import SliderSectionContainer from './components/SliderSection/SliderSectionContainer'
+import ProfileSectionContainer from './components/ProfileSection/ProfileSectionContainer'
+import { getProfileNameThunk, getFavoriteListThunk, getHistoryListThunk, setFavoriteListThunk, setHistoryListThunk } from './state/thunks'
+import { getProfileName, getProfileFavorite, getProfileHistory } from './state/selectors'
 import logo from './assets/icons/logo.svg'
 
-function App() {
+function App({ favorite, history, getProfileNameThunk, getFavoriteListThunk, setFavoriteListThunk, getHistoryListThunk, setHistoryListThunk }) {
+
+  useEffect(() => {
+    getProfileNameThunk()
+    getFavoriteListThunk()
+    getHistoryListThunk()
+  }, [getProfileNameThunk, getFavoriteListThunk, getHistoryListThunk])
+
+  useEffect(() => {
+    if (favorite) setFavoriteListThunk(favorite)
+  }, [favorite, setFavoriteListThunk])
+
+  useEffect(() => {
+    if (history) setHistoryListThunk(history)
+  }, [history, setHistoryListThunk])
+
   return (
     <>
       <Header icon={logo} title='Movie App' />
